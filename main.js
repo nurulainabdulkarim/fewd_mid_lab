6f69ca3b73e4ccba9c5e8f06014fcbf5b225f623
nurulainabdulkarim <nurulainabdulkarim96@gmail.com>
2021-08-18 17:47:32 +0800

    start js
    start js

---
+++ main.js
@@ -0,0 +1,11 @@
+let modal = document.querySelector(".modal")
+let openModal = document.querySelector(".btn")
+let closeModal = document.querySelector(".close")
+
+//Triggered Modal
+document.querySelector(".btn").addEventListener('click', open)
+
+//Function triggered Modal
+function open(){
+    console.log(123)
+}
