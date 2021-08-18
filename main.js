5f6b6511c3396468f573686c682820ed9a24f4e7
nurulainabdulkarim <nurulainabdulkarim96@gmail.com>
2021-08-18 18:26:05 +0800

    jss
    jss

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
-let modal = document.querySelector(".modal")
-let openModal = document.querySelector(".btn")
-let closeModal = document.querySelector(".close")
+let modal = document.querySelector(".modal");
+let openModal = document.querySelector(".btn");
+let closeModal = document.querySelector(".close");
+let closeContinue = document.querySelector(".continue_btn");
 
-//Triggered Modal
-document.querySelector(".btn").addEventListener('click', open)
 
-//Function triggered Modal
-function open(){
-    console.log(123)
+//Listen for open click
+document.querySelector(".btn").addEventListener('click', open);
+
+//Function for open click
+function open() {
+    modal.style.display = "block"
+}
+
+//Listen for close click
+document.querySelector(".close").addEventListener('click', close);
+
+//Function for close click
+function close() {
+    modal.style.display = "none"
+}
+
+//Listen for continue button click
+document.querySelector(".continue_btn").addEventListener('click', ctn);
+
+//Function for close click
+function ctn() {
+    modal.style.display = "none"
+}
+
+//Listen if the cursor click at the window
+window.addEventListener('click', close2);
+
+//Function for close click at the window
+function close2(a){
+    if (a.target == modal){
+        modal.style.display = 'none';
+    }
 }
