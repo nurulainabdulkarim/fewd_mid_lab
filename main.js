d61f92b7767c52f45d39d71ffa3d534de9d19582
nurulainabdulkarim <nurulainabdulkarim96@gmail.com>
2021-08-19 15:34:51 +0800

    jss button to red
    jss button to red

--- main.js
+++ main.js
@@ -37,3 +37,46 @@ function close2(a){
         modal.style.display = 'none';
     }
 }
+
+// ---------------------HOMEWORK START HERE-----------------------
+// * Easiest
+// * 1) Get the title for each dog/cat services, and put them into the console using console.log
+document.querySelector(".header1");
+console.log("Dog services")
+
+// * 2) Change the title of the page to "We're the CAT People"
+document.querySelector(".catpeople").innerHTML ="We're The Cat People";
+
+// * 3) Change the main image to be something cat related.
+
+let image_tracker = 'dog'
+function cat(){
+    var image = document.querySelector(".myImg");
+    // image.src = "img/cat and woman.jpg"
+    if (image_tracker =='dog'){
+        image.src = "img/cat and woman.jpg";
+        image_tracker ='cat'
+    }else{
+        image.src = "img/hero.jpg";
+        image_tracker ='dog'
+    }
+}
+    
+// * 
+// * 
+// * Harder
+// * 1) Replace the call to action button background color with "red"
+ let bookbutton = document.querySelector(".btn2")
+ bookbutton.addEventListener('click', () => bookbutton.style.backgroundColor='red')
+
+// * 2) Find another image and replace the header (try unsplash.com, get familiar with searching for images)
+// * 3) When someone clicks the "Book your next Rover sitter" button, change the text of the button to say "Thanks"
+// * 
+// * 
+// * Hardest (only if you get the others first)
+// * 1) Make the responsive menu work!
+// * 2) Implement a modal window - "Sign up now" CTA when someone clicks the 'Sign Up' button
+
+
+
+// src="img/cat and woman.jpg"
